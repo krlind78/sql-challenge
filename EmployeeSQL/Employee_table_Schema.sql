a3068e5d0aef27473affd5a86e08889fefb8f517
Kevin <krlind78@yahoo.com>
2023-08-12 22:14:50 -0400

    updated

--- EmployeeSQL/Employee_table_Schema.sql
+++ EmployeeSQL/Employee_table_Schema.sql
@@ -3,7 +3,7 @@ DROP TABLE departments
 
 CREATE TABLE departments (
     dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL
+    dept_name VARCHAR   NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
@@ -12,27 +12,27 @@ select * from departments
 --Create employees table
 DROP TABLE employees
 
-CREATE TABLE employees (
+CREATE TABLE employees(
     emp_no INT NOT NULL,
-	emp_title VARCHAR NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
     birth_date DATE NOT NULL,
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
     sex VARCHAR NOT NULL,
-    hire_date DATE   NOT NULL
-	PRIMARY KEY (emp_no)
+    hire_date DATE   NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
+	PRIMARY KEY(emp_no)
+	
 );
-
-select * from employees
 
 -- Create dept_emp table
 DROP TABLE dept_emp
 
-CREATE TABLE dept_emp (
-    emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+CREATE TABLE dept_emp(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
 select * from dept_emp
@@ -42,7 +42,7 @@ DROP TABLE dept_manager
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR   NOT NULL,
-    emp_no INT   NOT NULL
+    emp_no INT   NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -55,7 +55,7 @@ DROP TABLE salaries
 
 CREATE TABLE salaries (
     emp_no INT   NOT NULL,
-    salary BIGINT   NOT NULL
+    salary BIGINT   NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
@@ -66,10 +66,9 @@ select * from salaries
 DROP TABLE titles
 
 CREATE TABLE titles (
-    title_id VARCHAR   NOT NULL,
-    title VARCHAR   NOT NULL
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
-
+	title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+	PRIMARY KEY (title_id)
 );
 
 select * from titles
